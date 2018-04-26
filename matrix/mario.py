5b95b75294b1e4fdffcb23c3968c5f260e2177a3
basismit <basismit@student.artevelde.be>
2018-04-26 21:39:24 +0200

    Add mario matrix jump

--- matrix/mario.py
+++ matrix/mario.py
@@ -5,23 +5,49 @@ import os
 
 sense = SenseHat()
 
-black = (0, 0, 0)
-blue = (0, 0, 255)
+Bk = (0,0,0)
+Wh = (255,255,255)
+Rd = (255, 0, 77)
+Bl = (41, 173, 255)
+Br = (187, 90, 59)
+Pk = (255, 208, 174)
+Yl = (250, 252, 44)
 
-x_pos = 0
-y_pos = 0
-n_rows = 7
-n_cols = 7
-clear_before_every_loop = True
+mario = [
+    Bk, Bk, Bk, Rd, Rd, Rd, Wh, Bk,
+    Bk, Bk, Bk, Rd, Rd, Rd, Rd, Rd,
+    Bk, Bk, Br, Pk, Br, Bk, Pk, Bk,
+    Bk, Bk, Br, Pk, Pk, Br, Br, Pk,
+    Bk, Bk, Bk, Br, Pk, Pk, Pk, Bk,
+    Bk, Rd, Rd, Yl, Bl, Bl, Yl, Bk,
+    Wh, Bk, Bl, Bl, Bl, Bl, Bk, Bl,
+    Bk, Bk, Br, Bk, Bk, Bk, Rd, Bk  
+    
+]
 
-while x_pos <= n_cols and y_pos <= n_rows:
+mario_jump = [
+    Bk, Bk, Bk, Rd, Rd, Rd, Rd, Rd,
+    Bk, Bk, Br, Pk, Br, Bk, Pk, Bk,
+    Bk, Bk, Br, Pk, Pk, Br, Br, Pk,
+    Bk, Bk, Bk, Br, Pk, Pk, Pk, Bk,
+    Wh, Rd, Bl, Yl, Bl, Bl, Yl, Bl,
+    Bk, Bk, Br, Bk, Bk, Bk, Rd, Bk,  
+    Bk, Bk, Bk, Bk, Bk, Bk, Bk, Bk,   
+    Bk, Bk, Bk, Bk, Bk, Bk, Bk, Bk    
+]
+
+jump = False;
+
+while True:
   try:
-    if clear_before_every_loop:
-      sense.clear()
-    sense.set_pixel(x_pos, y_pos, blue)
-    y_pos = (y_pos + 1) if (x_pos + 1 == n_cols + 1) else (y_pos)
-    x_pos = (x_pos + 1) if (x_pos + 1 <= n_cols) else (0)
-    sleep(0.1)
+    if jump:
+      sense.set_pixels(mario_jump)
+    else :
+      sense.set_pixels(mario)
+  
+    sleep(1)
+    jump = True if (jump == False) else False
+
   except KeyboardInterrupt:
     print('Interrupted')
     sense.clear()
